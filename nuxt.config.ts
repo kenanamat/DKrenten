798d80fbb018d0eae99991733899455bbacede78
Kongonon <33282458+kenanamat@users.noreply.github.com>
2023-04-23 11:42:38 +0200

    removed vuefire

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,7 +6,6 @@ export default defineNuxtConfig({
     "@nuxtjs/tailwindcss",
     "@nuxt/devtools",
     "nuxt-icon",
-    "nuxt-vuefire",
     "@nuxtjs/supabase",
     'nuxt-headlessui'
   ],
@@ -25,20 +24,6 @@ export default defineNuxtConfig({
         require("@tailwindcss/forms"),
       ]
     },
-  },
-  vuefire: {
-    config: {
-      apiKey: "AIzaSyAIwAhhVMUIH4-fT-qloGWkYmZV5BOT2tk",
-      authDomain: "dkrenten-4548a.firebaseapp.com",
-      projectId: "dkrenten-4548a",
-      storageBucket: "dkrenten-4548a.appspot.com",
-      messagingSenderId: "650477249295",
-      appId: "1:650477249295:web:b7937adfba2ba94876d47a",
-      measurementId: "G-WPSLFEXZZ3"
-    },
-    admin: {
-      serviceAccount: "./dkrenten-4548a-firebase-adminsdk-y94ar-0bc8f2aaad.json"
-    }
   },
   nitro: {
     prerender: {
