165e7e5548709805b5dbe414e4fd5cfbff699b17
Kongonon <33282458+kenanamat@users.noreply.github.com>
2023-05-29 14:16:32 +0200

    fixed different hover and scroll

--- components/NaNek.vue
+++ components/NaNek.vue
@@ -5,7 +5,7 @@
   >
     <a
       href="mailto:info@nanek.nl"
-      class="flex cursor-pointer items-center gap-2 rounded-full bg-[#272640] py-2 transition-all group-hover:py-1 group-hover:px-3 shadow-2xl"
+      class="flex cursor-pointer items-center gap-2 rounded-full bg-[#272640] py-2 shadow-2xl transition-all group-hover:py-1 group-hover:px-3"
       :class="[scroll > 0 && 'pl-0 pr-2', scroll === 0 && 'px-3 !py-1']"
     >
       <p
@@ -20,7 +20,7 @@
       <img
         src="/NaNek-logo.png"
         alt=""
-        class="transition-all group-hover:h-12 max-sm:h-6"
+        class="transition-all group-hover:h-10 max-sm:h-6"
         :class="[scroll > 0 && 'h-8', scroll === 0 && '!h-10']"
       />
     </a>
